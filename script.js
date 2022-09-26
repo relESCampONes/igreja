34e4e7498fe92041729105c20b60a31cfd9d8edd
relESCampONes <wellington_lima@live.com>
2022-09-25 23:27:19 -0300

    correct

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const open = document.getElelmentById('open')
+const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
